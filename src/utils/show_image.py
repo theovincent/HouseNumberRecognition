7b2028218361169f05f8566c3a576bd73b2c14a5
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2020-03-28 14:21:59 +0100

    Utils file

---
+++ src/utils/show_image.py
@@ -0,0 +1,28 @@
+"""
+This code plot an array image and indicates the label with the index of the image.
+"""
+import matplotlib.pyplot as plt
+import numpy.random as rd
+
+
+def show_image(image, index, label, real_label):
+    """
+    Plots the image and indicate the index and the label.
+
+    Args:
+        image (array, any shape with at least 2 dimensions): the image.
+
+        index (int): the index of the image.
+
+        label (int): the estimated label of the image.
+
+        real_label (int): the true label of the image.
+    """
+    plt.imshow(image)
+    plt.title('Image {}, Estimated label : {}, True label : {}'.format(index, label, real_label))
+    plt.show()
+
+
+if __name__ == "__main__":
+    IMAGE = rd.randint(0, 255, (32, 32, 3))
+    show_image(IMAGE, 90, 1, 2)
